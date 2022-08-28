97922ab75be00a364bc2ba7f3d8a7cda8fcb7adb
Coolthulhu <Coolthulhu@users.noreply.github.com>
2022-08-28 14:15:45 +0000

    deploy: b5088e4a4b65c2faffaa10703b290acc399da57d

--- structjmapgen__int.js
+++ structjmapgen__int.js
@@ -4,8 +4,8 @@ var structjmapgen__int =
     [ "jmapgen_int", "structjmapgen__int.html#a3eeed3ec5ac1d68b60c5dd678102187a", null ],
     [ "jmapgen_int", "structjmapgen__int.html#a7d2f43f8f1c0fa5f9409f8562f5e579e", null ],
     [ "jmapgen_int", "structjmapgen__int.html#afda6a32f9db6b0190e0ddf927c1b0839", null ],
-    [ "jmapgen_int", "structjmapgen__int.html#ab5d661168459112504623684d209a3e2", null ],
+    [ "jmapgen_int", "structjmapgen__int.html#a1b31ac6bf035526128161f064adf14bb", null ],
     [ "get", "structjmapgen__int.html#af91db998b4c873453177d97ad575d138", null ],
-    [ "val", "structjmapgen__int.html#a419ab06da234974d3cd29fdb9cc3589e", null ],
-    [ "valmax", "structjmapgen__int.html#af46ca2297a74d2a89244702ca1c6fa04", null ]
+    [ "val", "structjmapgen__int.html#a39b7948e4cf4fb14e9f55a773282ae4f", null ],
+    [ "valmax", "structjmapgen__int.html#add599bedfbdd4308aed5a07a551e28be", null ]
 ];
